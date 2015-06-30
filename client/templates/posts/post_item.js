d4fc18da823d264a4aaf4c2ab497173d43711f44
Moufid Larbi <moufid4@gmail.com>
2015-06-30 19:49:29 +0100

    Added post reordering animation.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -17,6 +17,24 @@ Template.postItem.helpers({
 	}
 });
 
+Template.postItem.rendered = function(){
+  var instance = this;
+  var rank = instance.data._rank;
+  var $this = $(this.firstNode);
+  var postHeight = 80;
+  var newPosition = rank * postHeight;
+  if (typeof(instance.currentPosition) !== 'undefined') {
+    var previousPosition = instance.currentPosition;
+    var delta = previousPosition - newPosition;
+    $this.css("top", delta + "px");
+    
+  }
+  Meteor.defer(function() {
+    instance.currentPosition = newPosition;
+    $this.css("top",  "0px");
+  }); 
+};
+
 Template.postItem.events({
 	'click .upvotable': function(e) {
 		e.preventDefault();
